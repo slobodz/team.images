20718c6caed66f263b9db56b912530a7eda674f4
Slawek <s.e.pietrzyk@gmail.com>
2018-10-03 23:15:49 +0200

    fix

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     keywords = "example documentation tutorial",
     url = "http://packages.python.org/team.datasync",
     install_requires=reqs('requirements.txt'),
-    packages=find_namespace_packages(include=['team', 'team.*']),#['team.images','team.images.entity','team.images.service','team.images.templates'],
+    packages=find_namespace_packages(include=['team', 'team.*']),
     dependency_links=['https://teampypi.herokuapp.com/'],    
     #namespace_packages=['team'],
     long_description=read('README.md'),
